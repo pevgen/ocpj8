eb232e8abfa0171ee353e46d575ac493ab491bf0
pevgen <pevgen@gmail.com>
2017-01-11 19:26:33 +0300

    Project initialization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ocpj8
 Examples for my preparation to exam OCP J8
+
+#### 2017-01-11
+Project initialization
---
+++ src/ml/pevgen/ocpj8/Main.java
@@ -0,0 +1,8 @@
+package ml.pevgen.ocpj8;
+
+/**
+ * @author Polyak Evgeny
+ * @since 11.01.2017
+ */
+public class Main {
+}
