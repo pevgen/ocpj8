4b1648e184925f55b773354259654aad46743ebb
pevgen <pevgen@gmail.com>
2017-01-12 14:31:01 +0300

    Project initialization

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ocpj8
 Examples for my preparation to exam OCP J8
 
-#### 2017-01-11
+## 2017-01-12
+  - add ml.pevgen.ocpj8.ch1.OuterAndInner class (it shows difficulties with inner classes)
+  
+## 2017-01-11
 Project initialization
+
---
+++ src/ml/pevgen/ocpj8/ch1/OuterAndInner.java
@@ -0,0 +1,45 @@
+package ml.pevgen.ocpj8.ch1;
+
+/**
+ * OCP J8. Chapter 01.
+ *
+ * Example of inner (instance and static) classes and variables
+ *
+ * @author Polyak Evgeny
+ * @since 12.01.2017
+ */
+public class OuterAndInner {
+
+    /**
+     * Inner INSTANCE class has some limitations
+     */
+    public class InnerInstance {
+        // public static String innerStaticVariable = "static variable in instance inner class"; // NOT COMPILE  as Inner - instance class
+        // public static void innerStaticMethod(){}                                              // NOT COMPILE  as Inner - instance class
+        public String innerInstanceVariable;
+        public void innerInstanceMethod(){}
+    }
+
+    /**
+     * Inner STATIC class
+     */
+    public static class InnerStatic {
+        public static String innerStaticVariable = "static variable in static inner class"; // OK
+        public static void innerStaticMethod(){}                                            // OK
+        public String innerInstanceVariable;
+        public void innerInstanceMethod(){}
+    }
+
+    public static void main(String[] args) {
+        OuterAndInner outerAndInner = new OuterAndInner();
+
+        // with new OuterAndInner(), as it is instance inner class
+        OuterAndInner.InnerInstance innerInstance = new OuterAndInner().new InnerInstance();
+
+        // creation Inner static class
+        OuterAndInner.InnerStatic innerStatic = new OuterAndInner.InnerStatic();
+        OuterAndInner.InnerStatic innerStatic2 = new InnerStatic();
+
+
+    }
+}
